9f003d78017bf8bab3b7bdc43b100d39687f3379
AShakhmatov <Coluchy@yandex.ru>
2024-01-12 02:33:34 +0600

    added changes after review

---
+++ src/test/java/github/DragDrop.java
@@ -0,0 +1,38 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragDrop {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+    }
+
+    @Test
+    void DragDropAToBAction() {
+
+        open("/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void DragDropAToBElement() {
+        open("/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
---
+++ src/test/java/github/Hover.java
@@ -0,0 +1,20 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Hover {
+
+    @Test
+    void shouldFindPageWithHeading() {
+        // open selenide repository
+        open("https://github.com/");
+        $("ul.d-lg-flex.list-style-none").$(byText("Solutions")).hover();
+        $("[href='/enterprise']").click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
+
+    }
+}
